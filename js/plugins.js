b0c4284029a9a22c5c36b73bd31fcb0642e9bf77
MrZech <68412499+MrZech@users.noreply.github.com>
2023-06-13 07:54:52 -0500

    Add files via upload

--- js/plugins.js
+++ js/plugins.js
@@ -42,7 +42,7 @@
                 adaptiveHeight: false,
                 appendArrows: $(element),
                 appendDots: $(element),
-                arrows: true,
+                arrows: false,
                 asNavFor: null,
                 prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button">Previous</button>',
                 nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button">Next</button>',
